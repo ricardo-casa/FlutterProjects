8cd05ed1745d94023735a6c4bd8b1dcf751f7bab
Ricardo Carrillo <ricardo.carrsan8@gmail.com>
2024-05-04 22:11:25 -0600

    adding current turn name correction

--- lib/screens/ticTacToeGame.dart
+++ lib/screens/ticTacToeGame.dart
@@ -13,28 +13,19 @@ class _TicTacToeGameState extends State<TicTacToeGame> {
   final List<String> _players = ['X', 'O'];
   int _currentPlayer = 0;
 
-  int getCurrentPlayerTurn() => _currentPlayer % 2;
+  int _getCurrentPlayerTurn() => _currentPlayer % 2;
 
   @override
   Widget build(BuildContext context) {
     final arguments = (ModalRoute.of(context)?.settings.arguments ?? <String, dynamic>{}) as Map;
     String playerOneName = arguments['name1'] ?? "Jugador 1";
-    String planerTwoName = arguments['name2'] ?? "Jugador 2";
-
-    String name = 'null';
-
-    if (_currentPlayer == 0){
-      String name = arguments['name1'] ?? "Jugador 1";
-    } else {
-      String name = arguments['name2'] ?? "Jugador 2";
-    }
+    String playerTwoName = arguments['name2'] ?? "Jugador 2";
+    String nameToDisplay = _getCurrentPlayerTurn() == 0 ? playerOneName : playerTwoName;
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Turno de: $name'),
-
+        title: Text('Turno de $nameToDisplay'),
       ),
-
       body: Container(
           padding: const EdgeInsets.fromLTRB(20, 30, 20, 30),
           child: Column(children: [
@@ -46,9 +37,10 @@ class _TicTacToeGameState extends State<TicTacToeGame> {
                   if (_board[row][cell] == '') {
                     _board[row][cell] = _players[_currentPlayer % 2];
                     _currentPlayer++;
+                    print('TABLERO ACTUAL: $_board');
                   }
                 });
-              }, currentPlayer: getCurrentPlayerTurn(),
+              }, currentPlayer: _getCurrentPlayerTurn(),
             ),
             const SizedBox(height: 100),
             ElevatedButton(
